41bef84c60475c380eeee2b0bf8c1b467f0ad9ba
SabrinaKaren <sabrinaa.karen@hotmail.com>
2022-01-28 20:36:27 -0300

    Dynamodb inclusão de autoscale

--- src/main/java/com/myorg/DdbStack.java
+++ src/main/java/com/myorg/DdbStack.java
@@ -1,13 +1,16 @@
 package com.myorg;
 
 import software.constructs.Construct;
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.EnableScalingProps;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.UtilizationScalingProps;
 
 public class DdbStack extends Stack {
 
@@ -37,6 +40,26 @@ public class DdbStack extends Stack {
                 .timeToLiveAttribute("ttl")
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
+
+        productEventsDdb.autoScaleReadCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
+        productEventsDdb.autoScaleWriteCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
 
     }
 
